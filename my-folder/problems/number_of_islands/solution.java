80da990d1232f3afc9e65b7d16915f32b29bc8c5
Hettgen <134515214+Hettgen@users.noreply.github.com>
2024-05-17 04:56:32 +0000

    Sync LeetCode submission - Number of Islands (java)

---
+++ my-folder/problems/number_of_islands/solution.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int counter = 0;
+
+        for(int i = 0; i < grid.length; i++){
+            for(int y = 0; y < grid[i].length; y++){
+                if(grid[i][y] == '1'){
+                    System.out.println("i: " + i + "\ny: " + y);
+                    search(grid, i, y);
+                    counter++;
+                }
+            }
+        }
+        return counter;
+
+    }
+
+    public void search(char[][] grid, int x, int y){
+
+
+        if(x < grid.length  && y < grid[0].length && x >= 0 && y >= 0){
+            if(grid[x][y] == '1'){
+
+                grid[x][y] = '0';
+
+
+                search(grid, x, y+1);
+
+                search(grid, x+1, y);
+
+                search(grid, x-1, y);
+
+                search(grid, x, y-1);
+
+
+            }
+            
+        }
+        else{
+            return;
+        }
+        // if(grid[x][y] == '0'){
+        //     return;
+        // } R && D, L && U, 
+
+    }
+
+}
+
