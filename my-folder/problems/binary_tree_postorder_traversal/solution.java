aaed94c3ab4e611b718fe3d741361de4cd362436
Hettgen <134515214+Hettgen@users.noreply.github.com>
2024-07-30 17:35:30 +0000

    Sync LeetCode submission - Binary Tree Postorder Traversal (java)

---
+++ my-folder/problems/binary_tree_postorder_traversal/solution.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+
+        List<Integer> returnList = new ArrayList<Integer>();
+
+        traverse(root, returnList);
+
+        return returnList;
+
+
+    }
+
+
+    public static void traverse(TreeNode root, List<Integer> returnList){
+
+        if(root == null){
+            return;
+        }
+
+        traverse(root.left, returnList);
+        traverse(root.right, returnList);
+
+        returnList.add(root.val);
+
+    }
+}
